895af0a4936f0b65246a20d7952605f443fee580
Chalermpong Intarat <32490473+chalermpongintarat@users.noreply.github.com>
2022-03-11 15:54:44 +0700

    Add files via upload

---
+++ gcs_href.py
@@ -0,0 +1,18 @@
+with open("gene.txt", "r") as file: 
+    lines = file.read().split("\n")
+    find = '<a href="/cgi-bin/carddisp.pl?gene='
+    replace = '<a href="https://www.genecards.org/cgi-bin/carddisp.pl?gene='
+
+    href = [] 
+    for line in lines:
+        if find in line:
+            href_line = line.replace(find, replace)
+        else:
+            href_line = line
+        href.append(href_line)
+
+    f = open("gene_href.txt", "w")
+    for line in href:
+        f.write(str(line))
+        f.write("\n")
+    f.close()
